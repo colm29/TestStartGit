e224d66a096324579f6e58c8e7b37d1edd73a954
Colm Faherty <colm29@gmail.com>
2019-01-14 20:34:41 +0000

    Add project files.

---
+++ TestStartGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestStartGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
